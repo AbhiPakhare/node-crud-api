5bd34e02052e56e4979cf75ef6560fa6d1827b20
Abhishek Pakhare <47143014+AbhiPakhare@users.noreply.github.com>
2024-06-10 11:14:15 +0530

    Create product.controller.js

---
+++ controllers/product.controller.js
@@ -0,0 +1,58 @@
+const Product = require('../models/product.model.js')
+
+const index = async(req, res) => {
+    try {
+        const products = await Product.find({});
+        res.status(200).json(products)
+    } catch (error) {
+        res.status(500).json({
+            message : error.message
+        })
+    }
+}
+
+const create = async(req,res) => {
+    try {
+        const product = await Product.create(req.body);
+        res.status(200).json(product);
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }
+}
+
+const show = async(req, res) => {
+    try {
+        const { id } = req.params;
+        const product = await Product.findById(id);
+        res.status(200).json(product)
+    } catch (error) {
+        res.status(500).json({message:error.message})
+    }
+}
+
+const update = async(req, res) => {
+    try {
+        const { id } = req.params;
+        const product = await Product.findByIdAndUpdate(id, req.body);
+        if (!product) {
+            return res.status(404).json({message:"Product not found"})
+        }
+        const updatedProduct = await Product.findById(id);
+        res.status(200).json(updatedProduct);
+    } catch (error) {
+        res.status(500).json({message:error.message})
+    }
+}
+
+const deleteProduct = async(req, res) => {
+
+}
+
+
+module.exports = {
+    index,
+    create,
+    show,
+    update,
+    deleteProduct
+}
