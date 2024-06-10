fb3b72aa5faf96de12fc635191b40cb3c57059ec
Abhishek Pakhare <47143014+AbhiPakhare@users.noreply.github.com>
2024-06-10 11:16:38 +0530

    Create product.model.js

---
+++ models/product.model.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+
+
+const ProductScheme = mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'Product Name is required']
+    },
+    quantity: {
+        type: Number,
+        required: true,
+        default: 0
+    },
+    price: {
+        type: Number,
+        required: true,
+        default: 0
+    },
+    image: {
+        type: String,
+        required: false,
+    },
+   
+},{
+    timestamps: true,
+});
+
+const Product = mongoose.model('Product', ProductScheme);
+
+module.exports = Product;
